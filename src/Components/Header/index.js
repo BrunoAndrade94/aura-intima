34df777c5704acc8af3bea0a45af73ba6a4f6478
BrunoAndrade94 <brunozl@hotmail.fr>
2024-11-02 00:32:04 -0300

    projeto base

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,20 @@
-const Header=() => {
+import Logo from "../../../public/logo.png"
+
+
+const Header = () => {
     return (
         <>
-            <h1>
-                Header
-        </h1>
+            <h1 className="header">
+                <div className="container">
+                    <div className="row">
+                        <div className="logoWrapper col-sm-2">
+                            <Link to={"/"}><img src={Logo} alt='Logo' /></Link>
+                        </div>
+                    </div>
+                </div>
+            </h1>
         </>
     )
 }
 
-export default Header
+export default Header 
